c37666c377ea839a57bfb275dd953c1b54e96fd1
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-07-03 12:16:43 +0200

    actualizacion15

--- assets/js/indiceAlvearium.js
+++ assets/js/indiceAlvearium.js
@@ -3,9 +3,9 @@
 import THREE from './three.js';
 // import THREE from './path/to/three.js';
 // import { gsap } from '@vendor/gsap/gsap.min.js';
-// import { OrbitControls } from '../vendor/three/examples/jsm/controls/OrbitControls.js';
-// import { PointerLockControls } from '../vendor/three/examples/jsm/controls/PointerLockControls.js';
-// import { GLTFLoader } from '../vendor/three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from '../vendor/three/examples/jsm/controls/OrbitControls.js';
+import { PointerLockControls } from '../vendor/three/examples/jsm/controls/PointerLockControls.js';
+import { GLTFLoader } from '../vendor/three/examples/jsm/loaders/GLTFLoader.js';
 // import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import camera from './Camera2.js';
 import renderer from './Render.js';
