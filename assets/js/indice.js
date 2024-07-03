faa961caca33923699318b45e8379e44a64a228d
Judith Hoyuelos <teresajudithhoyuelosolalla_7254@escuelaces.net>
2024-07-03 10:38:57 +0200

    actualizacion3

--- assets/js/indice.js
+++ assets/js/indice.js
@@ -1,8 +1,8 @@
 // import * as THREE from '../vendor/three/build/three.module.js';
 
 import THREE from './three.js';
-
-const scene = new THREE.Scene();
+import scene from './Screen.js';
+// const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
